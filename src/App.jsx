bc3374d389d0bd4f4f99b6ddcc5e19654997f85c
sabreena <sabreena@axelerant.com>
2024-11-06 21:49:48 +0530

    LDT-544-Todolist: Todo list exercise.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,66 @@
+import { useState } from "react";
+import { TodoItem } from "./TodoItem";
+import "./styles.css";
+
 function App() {
-  return "Hello React!";
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addNewTodo() {
+    if (newTodo === "") return;
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodo, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setNewTodo("");
+  }
+
+  function toggleTodo(todoId, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === todoId) return { ...todo, completed };
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(todoId) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId);
+    });
+  }
+
+  return (
+    <>
+      <ul id="list">
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
+      </ul>
+
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addNewTodo}>Add Todo</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
