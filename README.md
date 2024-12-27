a6a212f526c3c422c5e2d43f0c8c5400bea30e6b
Sabreena <sabreena@Sabreenas-MacBook-Pro-1.local>
2024-12-27 16:45:50 +0530

    LDT-544-useLocalStorage: Practice useLocalStorage.

--- README.md
+++ README.md
@@ -12,7 +12,11 @@ Run the project:
 
 `npm run dev`
 
-# Project Structure
+# Task
 
-The `main` branch contains the project scaffold.
-For each exercise, a separate branch is created based on the course section.
+Create a custom useLocalStorage hook that functions identically to useState by returning an array where the first element is the value and the second element is the function to set the value. This hook should take two arguments. The first is a string which is the key for localStorage and the second is the initial value of the state.
+Whenever the state changes it should be synced with localStorage so that if you were to refresh your page nothing would change as all values are pulled from localStorage on initial load and stored in localStorage when changed.
+
+# Bonus
+Ensure that the useLocalStorage hook works just like useState in that you can pass it a value or function as the initial value.
+Use JSON to serialize and deserialize the values stored in localStorage so that it will work with any value (such as arrays or objects).
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,61 @@
+import { useLocalStorage } from "./useLocalStorage";
+
 function App() {
-  return "Hello React!";
+  const [firstName, setFirstName] = useLocalStorage("first_name", "")
+
+  const [lastName, setLastName] = useLocalStorage("last_name", () => {
+    return "Default"
+  })
+
+  const [hobbies, setHobbies] = useLocalStorage("HOBBIES", [
+    "Programming",
+    "Reading",
+  ])
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>First Name</label>
+        <input
+          type="text"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>Last Name</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+      </div>
+
+      <div>{hobbies.join(", ")}</div>
+      <button
+        onClick={() =>
+          setHobbies(currentHobbies => [...currentHobbies, "New Hobby"])
+        }
+      >
+        Add Hobby
+      </button>
+    </>
+  );
 }
 
 export default App;
