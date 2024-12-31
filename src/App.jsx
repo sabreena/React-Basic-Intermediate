c7810da953ce061fe35f93722841cf8fc421d31b
Sabreena <sabreena@Sabreenas-MacBook-Pro-1.local>
2024-12-31 19:37:17 +0530

    LDT-useContext: Practice useContext.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-function App() {
-  return "Hello React!";
-}
+import { useEffect, useState, createContext } from "react";
+import { Child } from "./Child";
 
-export default App;
+export const ThemeContext = createContext();
+
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function toggleTheme() {
+    setIsDarkMode((prevMode) => !prevMode);
+  }
+
+  useEffect(() => {
+    document.body.style.background = isDarkMode ? "#333" : "white";
+    document.body.style.color = isDarkMode ? "white" : "#333";
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      <Child />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident id
+        aliquam ea nesciunt qui ipsum. Magni iusto repellendus ut unde?
+      </p>
+    </ThemeContext.Provider>
+  );
+}
